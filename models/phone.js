5e416c4a1d229b1eb4f3cbe7cc9ff4887d422264
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-03-20 09:59:39 -0400

    initial commit

---
+++ models/phone.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const phoneSchema = new Schema({
+  brand: {type: String, required: [true, 'The phone brand is required']},
+  name: {type: String, required: [true, 'The phone name is required']},
+  image: {type: String, default: ''},
+  specs: {type: Array, default: []}
+
+});
+
+questionSchema.set('timestamps', true);
+
+const Phone = mongoose.model('Phone', phoneSchema);
+
+module.exports = Phone;
